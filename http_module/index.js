ae6c8d9a75e22dabe079ee98c839d843522471c8
testUserName <testEmail@gmail.com>
2025-09-13 11:10:06 +0100

    Express is a library that works on top of the http module. Helps developers with building request handlers.

--- http_module/index.js
+++ http_module/index.js
@@ -1,10 +1,12 @@
 const http = require("http");
-
-const PORT = process.env.PORT || 3000;
+const express = require("express")
 const users = require("../fixtures/users.json");
 const emails = require("../fixtures/emails.json");
 
-let server = http.createServer((req, res) => {
+const PORT = process.env.PORT || 3000;
+let app = express();
+
+app.use((req,res)=>{
   let route = req.method + " " + req.url;
   if (route === "GET /users") {
     res.end(JSON.stringify(users));
@@ -14,6 +16,8 @@ let server = http.createServer((req, res) => {
     res.end("You asked for " + route);
   }
 });
+
+let server = http.createServer(app);
 
 server.listen(PORT, (error) => {
   if (error) {
