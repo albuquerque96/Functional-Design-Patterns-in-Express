577fbe88b309589e512b6014a4a14024a1937b73
testUserName <testEmail@gmail.com>
2025-09-15 18:22:08 +0100

    Router Design Pattern removing the if .. else statements from the main middleware used to respond to all the requests made.  now each request handler is called according to the request characteristics.

--- http_module/index.js
+++ http_module/index.js
@@ -1,20 +1,31 @@
 const express = require("express");
 const users = require("../fixtures/users.json");
 const emails = require("../fixtures/emails.json");
-const {sendFormattedResponse} = require("./utils");
+const { sendFormattedResponse } = require("./utils");
 const PORT = process.env.PORT || 3000;
 let app = express();
+
+let getUsersRoute = (req, res) => {
+  res.send(users);
+};
+
+let getEmailsRoute = (req, res) => {
+  res.send(emails);
+};
+let noRouteFound = (req, res) => {
+  let route = req.method + " " + req.url;
+  res.end(res.end("You asked for " + route));
+};
+let routes = {
+  "GET /users": getUsersRoute,
+  "GET /emails": getEmailsRoute,
+};
 
 app.use((req, res) => {
   //console.log(req.headers.accept);
   let route = req.method + " " + req.url;
-  if (route === "GET /users") {
-   sendFormattedResponse(req, res,users);
-  } else if (route === "GET /emails") {
-    sendFormattedResponse(req,res,emails);
-  } else {
-    res.end("You asked for " + route);
-  }
+  let handler = routes[route]||noRouteFound;
+  handler(req, res);
 });
 
 app.listen(PORT, (error) => {
