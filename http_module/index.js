f1d07140b226eb3b4b61931e734d32661cd4f693
testUserName <testEmail@gmail.com>
2025-09-16 11:00:37 +0100

    How to define mutiple routes using the router object

--- http_module/index.js
+++ http_module/index.js
@@ -4,28 +4,34 @@ const emails = require("../fixtures/emails.json");
 const { sendFormattedResponse } = require("./utils");
 const PORT = process.env.PORT || 3000;
 let app = express();
+const router = express.Router();
 
 let getUsersRoute = (req, res) => {
   res.send(users);
 };
-
+let getUserRoute = (req, res) => {
+  let user = users.find((user) => user.id === req.params.id);
+  res.send(user);
+};
 let getEmailsRoute = (req, res) => {
   res.send(emails);
+};
+let getEmailRoute = (req, res) => {
+  let email = emails.find((email) => email.id === req.params.id);
+  res.send(email);
 };
 let noRouteFound = (req, res) => {
   let route = req.method + " " + req.url;
   res.end(res.end("You asked for " + route));
 };
-let routes = {
-  "GET /users": getUsersRoute,
-  "GET /emails": getEmailsRoute,
-};
-let router = (req, res) => {
-  //console.log(req.headers.accept);
-  let route = req.method + " " + req.url;
-  let handler = routes[route]||noRouteFound;
-  handler(req, res);
-}
+router.get("/users", getUsersRoute);
+router.get("/emails", getEmailsRoute);
+router.get("/users/:id", getUserRoute);
+router.get("/emails/from/:sender/to/:recipient", (req, res) => {
+  console.log(req.params);
+});
+router.get("/emails/:id", getEmailRoute);
+
 app.use(router);
 
 app.listen(PORT, (error) => {
